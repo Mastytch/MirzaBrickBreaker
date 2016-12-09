02b29d7695a0d1bdde79a052a45a77fa62f36d40
mastytchmac <mastytch@gmail.com>
2016-12-09 21:03:26 +0200

    init 2

--- src/ua/com/breaker/MapGenerator.java
+++ src/ua/com/breaker/MapGenerator.java
@@ -4,4 +4,11 @@ package ua.com.breaker;
  * Created by mastytchmac on 09.12.16.
  */
 public class MapGenerator {
+    public int map[][[];
+    public int brickWidth;
+    public int brickHeight;
+
+    public MapGenerator( int row, int col){
+        map = new int[row][col];
+    }
 }
